41a5133dbee0770af5d4cb9252d1dc78a79c5cff
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-03-07 18:05:51 -0500

    Refactor: removed show from the only statement bc we do not have a show action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  before_action :set_movie, only: [:show, :update, :destroy]
+  before_action :set_movie, only: [:update, :destroy]
 
   # GET /movies
   # GET /movies.json
